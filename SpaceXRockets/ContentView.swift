ba48a86297b200a1b4394b866dfefdeb3f059280
RIdwan Ayinla <ridwan.ayinla@apposing.co.uk>
2021-02-16 19:06:46 +0000

    Initial Commit

---
+++ SpaceXRockets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceXRockets
+//
+//  Created by Ridwan Ayinla on 16/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceXRockets/SpaceXRocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXRocketsApp.swift
+//  SpaceXRockets
+//
+//  Created by Ridwan Ayinla on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXRocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
